efb14f17dfcea346050abc8d44a75a33c439d881
Saika <kyon4501@gmail.com>
2020-12-08 12:58:21 +0800

    update feature/Complete_task_remove

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -4,4 +4,9 @@ window.addEventListener("DOMContentLoaded",function(){
     let li = even.target
     li.classList.toggle('checked')
   })
+  let x = document.querySelectorAll('.close')
+  x.forEach(y => { y.addEventListener('click',function(){
+    document.querySelector('li').remove()
+    })
+  });
 })
