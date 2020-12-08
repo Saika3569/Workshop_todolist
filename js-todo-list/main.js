d5fc93b1143cdf1f612a987a84240e8f9e628b86
Saika <kyon4501@gmail.com>
2020-12-08 11:52:12 +0800

    update task_add_checked

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,1 +1,7 @@
 // TO DO
+window.addEventListener("DOMContentLoaded",function(){
+  document.querySelector('ul').addEventListener('click',function(even){
+    let li = even.target
+    li.classList.toggle('checked')
+  })
+})
